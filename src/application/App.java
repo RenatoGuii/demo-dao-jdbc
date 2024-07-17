d0c2139798f3c873de4016a8ffe154b0fa726721
RenatoGuii <renatoguidev@gmail.com>
2024-07-17 11:48:01 -0300

    Reusing Instantiation

--- src/application/App.java
+++ src/application/App.java
@@ -6,10 +6,14 @@ import entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println();
+        System.out.println("=== TEST 1: seller findById ===");
+
         Seller seller = sellerDao.findByyId(3);
+        System.out.println();
 
         System.out.println(seller);
 
